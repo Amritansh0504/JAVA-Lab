502ce5e14315c5fd28d582e1ff83564bc114df2c
Amritansh Tandon <2205177@kiit.ac.in>
2024-01-22 06:12:51 +0530

    Added 2nd lab's 6th program

--- Lab 2/Lab2f.java
+++ Lab 2/Lab2f.java
@@ -1,7 +1,18 @@
-import java.util.Scanner;
 public class Lab2f {
+    
+    private static int i=0;
+    public Lab2f(){
+        i++;
+    }
+    public static int counter(){
+        return i;
+    }
+    
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        sc.close();
+        Lab2f obj1 = new Lab2f();
+        Lab2f obj2 = new Lab2f();
+        Lab2f obj3 = new Lab2f();
+        
+        System.out.println("No of objects created: "+counter());
     }
 }
