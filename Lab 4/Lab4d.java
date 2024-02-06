1e5f53c26cb1dcf52df2fae1d3561f0021aed713
Amritansh Tandon <2205177@kiit.ac.in>
2024-02-06 17:11:51 +0530

    updated 4d

--- Lab 4/Lab4d.java
+++ Lab 4/Lab4d.java
@@ -30,57 +30,28 @@ public class Lab4d {
    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        Person p1 = new Person();
-        Person p2 = new Person();
-        Person p3 = new Person();
-        
-        System.out.print("Enter the Name for user 1: ");
-        p1.name = sc.nextLine();
-        System.out.print("Enter the Aadhar no for user 1: ");
-        p1.aadhar_no = sc.nextInt();
-        System.out.print("Enter the Account number for user 1: ");
-        int a= sc.nextInt();
-        System.out.print("Enter the Balance for user 1: ");
-        int b = sc.nextInt();
-        p1.input(a, b);
-        sc.nextLine();
-        System.out.println();
-        
-        System.out.print("Enter the Name for user 2: ");
-        p2.name = sc.nextLine();
-        System.out.print("Enter the Aadhar no for user 2: ");
-        p2.aadhar_no = sc.nextInt();
-        System.out.print("Enter the Account number for user 2: ");
-        a= sc.nextInt();
-        System.out.print("Enter the Balance for user 2: ");
-        b = sc.nextInt();
-        p2.input(a, b);
-        sc.nextLine();
-        System.out.println();
-        
-        System.out.print("Enter the Name for user 3: ");
-        p3.name = sc.nextLine();
-        System.out.print("Enter the Aadhar no for user 3: ");
-        p3.aadhar_no = sc.nextInt();
-        System.out.print("Enter the Account number for user 3: ");
-        a= sc.nextInt();
-        System.out.print("Enter the Balance for user 3: ");
-        b = sc.nextInt();
-        p3.input(a, b);
-        System.out.println();
-        
+        Person p[] = new Person[3]; 
 
-        System.out.println("Details for user 1: ");
-        p1.disp();
-        System.out.println();
+        for(int i= 0; i<3; i++){
+            p[i] = new Person();
+            System.out.println("Enter the name for "+(i+1)+" user: ");
+            p[i].name = sc.nextLine();
+            System.out.print("Enter the Aadhar no for user 1: ");
+            p[i].aadhar_no = sc.nextInt();
+            System.out.print("Enter the Account number for user 1: ");
+            int a= sc.nextInt();
+            System.out.print("Enter the Balance for user 1: ");
+            int b = sc.nextInt();
+            p[i].input(a, b);
+            sc.nextLine();
+            System.out.println();
+        }
 
-        System.out.println("Details for user 2: ");
-        p2.disp();
-        System.out.println();
-        
-        System.out.println("Details for user 3: ");
-        p3.disp();
-        System.out.println();
+        for(int i=0; i<3; i++){
+            System.out.println("Details for user 1: ");
+            p[i].disp();
+            System.out.println();
+        }
         
         sc.close();
     } 
